bc51763d23627a2e2430952e22eb3b7219994ca2
Nandini Rao [nandinirao] <nandinirao@costco.com>
2022-10-30 17:04:33 -0700

    class 2 exercises

--- Class 2 Exercises and Homework.js
+++ Class 2 Exercises and Homework.js
@@ -6,18 +6,23 @@
 // - bestFriend (object with the same 3 properties as above)
 
 
-const myBestFriend = {firstName: 'Paul', 
-lastName: 'Smith', 
-'favorite food': 'Salad' }
+const bestFriend = {
+  firstName: 'Paul',
+  lastName: 'Smith',
+  'favorite food': 'Salad'
+}
 
-const me = { firstName: 'Nandini', 
-            lastName: 'Rao', 
-            'favorite food': 'Soup',
-            bestFriend: myBestFriend
+const me = {
+  bestFriend,
+  firstName: 'Nandini',
+  lastName: 'Rao',
+  'favorite food': 'Soup'
+
 }
 
 // 2. console.log best friend's firstName and your favorite food
-console.log(`My best friend's first name is ${me.bestFriend.firstName},
+const {firstName} = me.bestFriend;
+console.log(`My best friend's first name is ${firstName},
 My favorite food is ${me['favorite food']}
 `);
 
@@ -26,8 +31,8 @@ My favorite food is ${me['favorite food']}
 // -O-
 // -XO
 // X-X
-const ticTacToe = [['-','O', '-'],
-['-', 'O', 'X'],
+const ticTacToe = [['-', 'O', '-'],
+['-', 'X', 'O'],
 ['X', '-', 'X']];
 
 console.table(ticTacToe);
@@ -35,7 +40,7 @@ console.table(ticTacToe);
 // 4. After the array is created, 'O' claims the top right square.
 // Update that value.
 ticTacToe[0][2] = 'O';
-console.log(ticTacToe[0]);
+console.log(`updated top row: ${ticTacToe[0]}`);
 
 // 5. Log the grid to the console.
 console.table(ticTacToe);
@@ -47,21 +52,40 @@ console.table(ticTacToe);
 // - Use rubular to check a few emails: https://rubular.com/
 // - Use regexp test method https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
 
-// example: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-const emailRegex = //;
-console.log(emailRegex.test('abc@test.com'));
-console.log(emailRegex.test('abctest.com'));
-console.log(emailRegex.test('abc@testcom'));
+const emailRegex = /^[a-z0-9]+@[a-z0-9]+\.[a-z]+$/i;
+//true
+let myEmail = 'abc@test.com';
+console.log(emailRegex.test(myEmail));
+myEmail = 'ABC12@Test.NET';
+console.log(emailRegex.test(myEmail));
+
+//false
+myEmail = 'abctest.com';
+console.log(emailRegex.test(myEmail));
+myEmail = 'abc@testcom';
+console.log(emailRegex.test(myEmail));
+myEmail = 'abc@_test.com';
+console.log(emailRegex.test());
+myEmail = ' abc@test.com '
+console.log(emailRegex.test(myEmail));
 
 // 7. You are given an assignmentDate as a string in the format "month/day/year"
 // i.e. '1/21/2019' - but this could be any date.
 // Convert this string to a Date
 const assignmentDate = '1/21/2019';
+const dateRegex = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
 
+let [dateString, month, day, year] = dateRegex.exec(assignmentDate);
+console.log(`assigmentDate: ${dateString}, month: ${month}, day: ${day}, year: ${year}`);
+const convertedAssignmentDate = new Date(year, month -1, day);
+console.log(`converted assignmentDate: ${convertedAssignmentDate}`);
 
 // 8. Create a new Date instance to represent the dueDate.  
 // This will be exactly 7 days after the assignment date.
+const dueDate = new Date(convertedAssignmentDate);
+dueDate.setDate(dueDate.getDate() + 7);
 
+console.log(`dueDate: ${dueDate}`);
 
 // 9. Use dueDate values to create an HTML time tag in format
 // <time datetime="YYYY-MM-DD">Month day, year</time>
@@ -80,6 +104,9 @@ const months = [
   'November',
   'December'
 ];
+const [dueDateMonth, dueDateDate, dueDateFullYear] = [dueDate.getMonth(), dueDate.getDate(), dueDate.getFullYear()];
 
+const timeTag = `<time datetime="${dueDateFullYear}-${dueDateMonth+1}-${dueDateDate}">${months[dueDateMonth]} ${dueDateDate}, ${dueDateFullYear}</time>`;
 
 // 10. log this value using console.log
+console.log(`time tag: ${timeTag}`);
